1d5aaa5f2683f30bdac6a60989373c9efde12221
gabrielkerr <mgabrielkerr@gmail.com>
2016-10-31 10:27:59 -0600

    Update with more prompts and simple flow

--- app.js
+++ app.js
@@ -28,10 +28,39 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.DialogAction.send("Hi there!");
+bot.Dialog
 
-bot.dialog('/', [function (session) {
-  session.send("You're amazing.");
+bot.dialog('/', [
+    function (session) {
+      builder.Prompts.text(session, "Hi there! What is your name?");
+    },
+    function(session, results){
+      session.send("Why hello " + results.response + "!");
+      builder.Prompts.number(session, " I can send you an army of robots to do your bidding. How many robots would you like?");
+    },
+    function(session, results){
+      session.send("And " + results.response + " robots you shall receive!");
+      builder.Prompts.choice(session, "Your robot army can do one of three things. Pick which programming will best fit your purposes.", "Build|Destroy|Save the World|Hand out Ice Cream");
+    },
+    function(session, results){
+      session.send("A wise choice! Your robot army will be built to " + results.response.entity + ".");
+      builder.Prompts.time(session, "What time would you like your robot army to arrive?");
+    },
+    function(session, results){
+      session.send("Your army shall arrive promptly at " + results.response +  "!");
+      builder.Prompts.confirm(session, "Thank you for your request! Will you require another army in the future?");
+    },
+    function(session, results){
+      session.send(results.response);
+      if(results.response == 'confirm_yes')
+      {
+        session.send("Great! I'm looking forward to seeing you again!");
+      }
+      else(results.response == 'confirm_no')
+      {
+        session.send("No? Well I hope you had a good experience!");
+      }
+    }
 ]);
 
 
